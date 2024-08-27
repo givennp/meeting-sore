6abbc2bf49ee3416c80e1b0e62c9fa9cb01a7ae6
givennp <givennatio2@gmail.com>
2024-08-28 01:12:38 +0700

    fix border radius input form

--- src/components/Input/index.js
+++ src/components/Input/index.js
@@ -4,13 +4,13 @@ const Input = ({ placeholder = "", className = "", textarea = false }) => {
       <div className="">
         {textarea ? (
           <textarea
-            className={`${className} resize-none peer py-3 px-0 text-body-04 max-md:text-body-05 md:min-w-[341px] max-md:w-full text-white outline-none focus:border-b-primary-brand duration-200 placeholder:duration-[200ms] font-normal block w-full bg-transparent border-t-transparent border-b-[1px] border-x-transparent border-b-white focus:border-t-transparent focus:border-x-transparent hover:placeholder:text-primary-brand disabled:opacity-50 disabled:pointer-events-none`}
+            className={`${className} input resize-none peer py-3 px-0 text-body-04 max-md:text-body-05 md:min-w-[341px] max-md:w-full text-white outline-none focus:border-b-primary-brand duration-200 placeholder:duration-[200ms] font-normal block w-full bg-transparent border-t-transparent border-b-[1px] border-x-transparent border-b-white focus:border-t-transparent focus:border-x-transparent hover:placeholder:text-primary-brand disabled:opacity-50 disabled:pointer-events-none`}
             placeholder={placeholder}
           />
         ) : (
           <input
             type="text"
-            className={`${className} peer py-3 px-0 text-body-04 max-md:text-body-05 md:min-w-[341px] max-md:w-full outline-none text-white focus:border-b-primary-brand duration-200 placeholder:duration-[200ms] font-normal block w-full bg-transparent border-t-transparent border-b-[1px] border-x-transparent border-b-white focus:border-t-transparent focus:border-x-transparent hover:placeholder:text-primary-brand disabled:opacity-50 disabled:pointer-events-none`}
+            className={`${className} input peer py-3 px-0 text-body-04 max-md:text-body-05 md:min-w-[341px] max-md:w-full outline-none text-white focus:border-b-primary-brand duration-200 placeholder:duration-[200ms] font-normal block w-full bg-transparent border-t-transparent border-b-[1px] border-x-transparent border-b-white focus:border-t-transparent focus:border-x-transparent hover:placeholder:text-primary-brand disabled:opacity-50 disabled:pointer-events-none`}
             placeholder={placeholder}
           />
         )}
