a7e192d618edd87a20f22e213c03725eb41ce307
givennp <givennatio2@gmail.com>
2024-09-09 02:15:06 +0700

    portfolio

---
+++ src/app/portfolio/page.js
@@ -0,0 +1,18 @@
+const Portfolio = () => {
+  return (
+    <div>
+      <div className="h-[72px]" />
+      <div>
+        <div>
+          <div>SOCIAL MEDIA</div>
+          <div>Markdata </div>
+        </div>
+        The Millennial Gen Z Kaltim project aims to build a media community
+        account for young people in East Kalimantan and gain at least 1,000
+        folowers within 3 months.
+      </div>
+    </div>
+  );
+};
+
+export default Portfolio;
